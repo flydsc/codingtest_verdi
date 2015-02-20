09d671a2c077a88ece1cf09384e1f0dcb585df27
Shichao Dong <u5135470@coplg20mac15.uds.anu.edu.au>
2015-02-21 09:59:55 +1100

    main file

--- testmain.py
+++ testmain.py
@@ -3,19 +3,26 @@ Created on 20/02/2015
 
 @author: dongshichao
 '''
+import zombie
 import numpy as np
+from creature import Creature
+
 dimension = 4
 grid = np.zeros((dimension,dimension))
-start = np.zeros(grid.shape)
-back = np.zeros(grid.shape)
-#print grid
-zombine = (1,1)
-poors = [(0,0),(2,2),(0,2)]
-grid[zombine] = 1
-for poor in poors:
-    grid[poor] = 2
-start[zombine] = -1
-#back = grid
-nearhood = []
+
+zombie_position = (1,1)
+poors_positions = [(0,0),(2,2),(0,2)]
+
+Zombie = zombie(zombie_position)
+victims =[]
+
+for vi in poors_positions:
+	temp = Creature(vi)
+	victims += temp
+
+grid[Zombie.current_position] = 1
+for i in victims:
+	grid[i.current_position] = 2
+
 print grid
 
